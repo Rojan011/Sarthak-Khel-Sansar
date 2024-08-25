e1af342599449c5ce9ec9be836624556844b0a0c
Rojan <shrestharojan79@gmail.com>
2024-08-25 14:14:32 +0545

    feature-events

---
+++ src/Eventsimg.js
@@ -0,0 +1,11 @@
+export const Eventsimg = [
+    {
+      image: "/assets/nban.jpg",
+    },
+    {
+      image: "/assets/nban2.jpg",
+    },
+    {
+      image: "/assets/nban3.jpg",
+    },
+  ];
